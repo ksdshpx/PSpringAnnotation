dcbba2905f06961bee88a3ecd7b31e04f48205f6
peng.xing <southeast_px@163.com>
2019-01-07 21:55:12 +0800

    组件注册-@Lazy-bean懒加载!

--- src/main/java/cn/ksdshpx/config/MainConfig2.java
+++ src/main/java/cn/ksdshpx/config/MainConfig2.java
@@ -2,6 +2,7 @@ package cn.ksdshpx.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 
 import cn.ksdshpx.beans.Person;
@@ -18,10 +19,17 @@ public class MainConfig2 {
 	 * prototype:多实例 (IOC容器启动不会调用方法创建对象放到IOC容器中，而是每次获取对象时会调用方法创建对象放到IOC容器中)
 	 * request:同一次请求创建一个实例 
 	 * session:同一个session创建一个实例
+	 * 
+	 * 
+	 * 懒加载：
+	 * 		单实例bean：默认在容器启动时创建对象
+	 *      懒加载：容器启动时不创建对象，而在第一次获取bean时创建对象，并进行一些初始化
 	 */
-	@Scope("prototype")
+	/*@Scope("prototype")*/
+	@Lazy
 	@Bean("person")
 	public Person person() {
+		System.out.println("往容器中添加person...");
 		return new Person("wangWu", 30);
 	}
 }
